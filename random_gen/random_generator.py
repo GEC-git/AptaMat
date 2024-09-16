ac5e0c2406be2a81842bae2b99a84f61e738309a
Bastien Cuvillier <94041896+GEC-git@users.noreply.github.com>
2024-09-16 16:00:56 +0200

    Changed the random gen to be more realistic

--- random_gen/random_generator.py
+++ random_gen/random_generator.py
@@ -1,7 +1,7 @@
 from random import random
 
 
-def random_gen(length=50, ratio_o_c=0.5, bias=0.6):
+def random_gen(length=1000, ratio_o_c=0.8, bias=0.6):
     """
         Generates a random dotbracket notation of length *length* and density *ratio_o_c*
     
@@ -13,41 +13,93 @@ def random_gen(length=50, ratio_o_c=0.5, bias=0.6):
     dot = "."
     open_bracket = "("
     closing_bracket= ")"
-
+    just_opened=False
     output=""
     nb_open_bracket=0
-    for i in range(length):
-        if nb_open_bracket>=length-i:
-            output+=closing_bracket
-        else:
-            r=random()
-            if r > ratio_o_c:
+    i=0
+    while i != length:
+        if length-i==2 or length-i==1 or length-i==0:
+            if nb_open_bracket==0:
                 output+=dot
                 ratio_o_c+=i/length**2
-            elif r >= 1-ratio_o_c:
-                output+=open_bracket
-                nb_open_bracket+=1
-                ratio_o_c-=(nb_open_bracket/length)
+                just_opened=False
+                i+=1
             else:
-                if nb_open_bracket==0:
-                    if random() >= bias:
-                        output+=open_bracket
-                        nb_open_bracket+=1
-                        ratio_o_c-=(nb_open_bracket/length)
-                    else:
-                        output+=dot
-                        ratio_o_c+=i/length**2
+                if just_opened:
+                    output+=dot
+                    ratio_o_c+=i/length**2
+                    just_opened=False
+                    i+=1
                 else:
-                    output+=closing_bracket
-                    nb_open_bracket-=1
+                    if nb_open_bracket>=length-i:
+                        output+=closing_bracket
+                        nb_open_bracket-=1
+                        just_opened=False
+                        i+=1
+                    else:
+                        if random() < bias:
+                            output+=dot
+                            ratio_o_c+=i/length**2
+                            just_opened=False
+                            i+=1
+                        else:
+                            output+=closing_bracket
+                            nb_open_bracket-=1
+                            just_opened=False
+                            i+=1
+        else:
+            if nb_open_bracket>=length-i and not(just_opened):
+                output+=closing_bracket
+                nb_open_bracket-=1
+                just_opened=False
+                i+=1
+            elif nb_open_bracket>=length-i and just_opened:
+                output+=dot
+                ratio_o_c+=i/length**2
+                just_opened=False
+                i+=1
+            else:
+                r=random()
+                if r > ratio_o_c:
+                    output+=dot
+                    ratio_o_c+=i/length**2
+                    just_opened=False
+                    i+=1
+                elif r >= 1-ratio_o_c:
+                    output+=open_bracket
+                    nb_open_bracket+=1
+                    just_opened=True
+                    ratio_o_c-=(nb_open_bracket/length)
+                    i+=1
+                else:
+                    if nb_open_bracket==0:
+                        if random() >= bias:
+                            output+=open_bracket
+                            nb_open_bracket+=1
+                            ratio_o_c-=(nb_open_bracket/length)
+                            just_opened=True
+                            i+=1
+                        else:
+                            output+=dot
+                            ratio_o_c+=i/length**2
+                            just_opened=False
+                            i+=1
+                    elif not(just_opened):
+                        output+=closing_bracket
+                        nb_open_bracket-=1
+                        just_opened=False
+                        i+=1
+                    
     return output
 
-def dotbracket_verif(input_str):
+def dotbracket_verif(input_str,length):
     """
         This function verifies if all the opened brackets are actually closed.
         It appears that sometimes, the output of random_gen is invalid, we want to ignore those outputs.
     """
     nb=0
+    if len(input_str)!=length:
+        return False
     for elt in input_str:
         if elt=="(":
             nb+=1
@@ -57,19 +109,21 @@ def dotbracket_verif(input_str):
     return not bool(nb)
 
 
-def file_struct_gen(nb_struct=20000):
+def file_struct_gen(nb_struct=5000):
     output=">Randomly generated structure file\n"
     i=0
+    lgth=1000
     while i < nb_struct:
-        gen=random_gen()
-        if dotbracket_verif(gen):
+        gen=random_gen(length=lgth)
+        if dotbracket_verif(gen,lgth):
             output+="--structure"+str(i+1)+"--\n"
             output+=gen+"\n"
             output+="[ "+str(random())+" ]\n"
             i+=1
+            
     return output
 
-f=open("RandomWeightFile20000-50.fa","a")
+f=open("RandomWeightFile5000-1000.fa","a")
 f.write(file_struct_gen())
 f.close()
 print("Successfuly created a randomly generated weighted structure file.")
