b7f197c7ab61308ca696bd677e0cf3bbd622af5e
Bastien Cuvillier <94041896+GEC-git@users.noreply.github.com>
2024-10-14 14:41:26 +0200

    Finished brute force method

--- aptalign/AptAlign.py
+++ aptalign/AptAlign.py
@@ -88,7 +88,7 @@ def arrangement(struct,max_size):
         return [struct_prealign]
     
     all_struct={struct_prealign:False}
-    
+    print("PASS1")
     temp_struct=struct_prealign
     dashes=find_dash(temp_struct)
     for i in range(fill):
@@ -97,9 +97,29 @@ def arrangement(struct,max_size):
             if not all_struct[elt]:
                 dashes=find_dash(elt)
                 all_struct[elt] = True
-                for elt1 in one_range(dashes,i,elt,all_struct):
+                res1=one_range(dashes,i,elt,all_struct)
+                for elt1 in res1:
                     all_struct[elt1]=False
-
+    
+    struct_prealign=struct+fill*"-"
+    
+    if find_dash(struct_prealign) == []:
+        return [struct_prealign]
+    
+    all_struct[struct_prealign]=False
+    print("PASS2")
+    temp_struct=struct_prealign
+    dashes=find_dash(temp_struct)
+    for i in range(fill):
+        print(round((i/fill)*100,3),"%")
+        for j,elt in enumerate(list(all_struct)):
+            if not all_struct[elt]:
+                dashes=find_dash(elt)
+                all_struct[elt] = True
+                res1=one_range(dashes,i,elt,all_struct)
+                for elt1 in res1:
+                    all_struct[elt1]=False
+    
     return all_struct
 
 def one_range(dashes,num,curr_struct,all_struct):
@@ -116,7 +136,7 @@ def one_range(dashes,num,curr_struct,all_struct):
         temp_struct=insert_str(temp_struct,i,"-")
         if temp_struct not in all_struct:
             if temp_struct not in res:
-                res.append(temp_struct)
+                res.append(temp_struct)           
     return res
 
 def calc_dist(elt1,l_struct2):
@@ -126,15 +146,16 @@ def calc_dist(elt1,l_struct2):
     return res
 
 def brute_force_calc(struct1,struct2,max_size=0):
-
+    
+    print(max_size)
     
     if len(struct1)>max_size: max_size=len(struct1)
     if len(struct2)>max_size: max_size=len(struct2)
     print("Generating arrangement")
     l_struct1=arrangement(struct1,max_size)
     l_struct2=arrangement(struct2,max_size)
-    print(l_struct1)
     dict_dist={}
+    print("S1: ",len(l_struct1),"| S2: ", len(l_struct2))
     print("This is a multiprocessed program, you have",mp.cpu_count(),"cores in your CPU.")
     nb=int(input("How much do you want to use? "))
     print("Creating pool on",nb,"cores.\n")
@@ -147,7 +168,7 @@ def brute_force_calc(struct1,struct2,max_size=0):
     # keep=min(dict_dist.keys())
     # return dict_dist[keep],keep
     if len(l_struct2) >= len(l_struct1):
-        res = pooling.starmap(calc_dist, [(elt1,l_struct1) for elt1 in l_struct2])
+        res = pooling.starmap(calc_dist, [(elt2,l_struct1) for elt2 in l_struct2])
     else:
         res = pooling.starmap(calc_dist, [(elt1,l_struct2) for elt1 in l_struct1])
     
@@ -158,9 +179,9 @@ def brute_force_calc(struct1,struct2,max_size=0):
         for elt1 in elt:
             res_fin.append(elt1)
     
+    min=res_fin[0][0]
     for i,elt in enumerate(res_fin):
-        min=np.inf
-        if elt[0]<min:
+        if elt[0]<=min:
             choose=elt
             min=elt[0]
     return choose
