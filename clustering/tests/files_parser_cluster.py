882c653534be4ef92022316d8726511d77098768
Bastien Cuvillier <94041896+GEC-git@users.noreply.github.com>
2024-10-16 16:12:24 +0200

    Adapted file parser cluster

--- clustering/tests/files_parser_cluster.py
+++ clustering/tests/files_parser_cluster.py
@@ -7,9 +7,9 @@ sys.path.append(root_path)
 
 import AptaFast as AF
 
-f=open("data_clustering_test.dat",'a')
+f=open("data_clustering_test_150x10.dat",'a')
 f.write("FAMILY    PDB_chain    SEQUENCE    DOTBRACKET\n")
-for i in range(1,30):
+for i in range(1,151):
     file = "/home/bcuvillier/Téléchargements/dbnFiles/bpRNA_tmRNA_"+str(i)+".dbn"
     lines = open(file).readlines()
     tbw=""
@@ -31,7 +31,7 @@ for i in range(1,30):
     f.write(tbw)        
 
 
-for i in range(1,30):
+for i in range(1,151):
     file = "/home/bcuvillier/Téléchargements/dbnFiles/bpRNA_CRW_"+str(i)+".dbn"
     lines = open(file).readlines()
     tbw=""
@@ -53,7 +53,7 @@ for i in range(1,30):
     f.write(tbw)
 
 
-for i in range(1,30):
+for i in range(1,151):
     file = "/home/bcuvillier/Téléchargements/dbnFiles/bpRNA_RFAM_"+str(i)+".dbn"
     lines = open(file).readlines()
     tbw=""
@@ -74,7 +74,7 @@ for i in range(1,30):
     tbw+="\n"
     f.write(tbw)
 
-for i in range(1,30):
+for i in range(1,151):
     file = "/home/bcuvillier/Téléchargements/dbnFiles/bpRNA_SRP_"+str(i)+".dbn"
     lines = open(file).readlines()
     tbw=""
