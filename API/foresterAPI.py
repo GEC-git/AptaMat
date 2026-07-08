b2c7ff728d01e6f66e3905fe789eca11ec0fc3d6
GEC UTC <94041896+GEC-git@users.noreply.github.com>
2026-07-08 08:58:56 +0200

    Add files via upload

--- API/foresterAPI.py
+++ API/foresterAPI.py
@@ -3,13 +3,13 @@ import subprocess
 
 def forester_pairwise(struct1,struct2):
 
-    tbw=">"+str(struct1.id)+"\n"
-    tbw+=str(struct1.sequence)+"\n"
-    tbw+=str(struct1.dotbracket)+"\n"
-    tbw+=">"+str(struct2.id)+"\n"
-    tbw+=str(struct2.sequence)+"\n"
-    tbw+=str(struct2.dotbracket)
-    filename="temp_struct"+str(struct2.id)+str(struct1.id)+str(os.getpid())+".fa"
+    tbw=">"+struct1.id+"\n"
+    tbw+=struct1.sequence+"\n"
+    tbw+=struct1.dotbracket+"\n"
+    tbw+=">"+struct2.id+"\n"
+    tbw+=struct2.sequence+"\n"
+    tbw+=struct2.dotbracket
+    filename="temp_struct"+struct2.id+struct1.id+str(os.getpid())+".fa"
     f_created=open(filename,'w+')
     f_created.write(tbw)
     f_created.close()
@@ -25,5 +25,7 @@ def forester_from_file(file):
     lines_from=open(file,'r').read()
     result=subprocess.run(["RNAforester","--fasta"],input=lines_from+"\n&",capture_output=True,text=True)
     lines=result.stdout.splitlines()
+    for i,line in enumerate(lines):
+        print(i,":",line)
     return lines[14].replace("\n",""),lines[16].replace("\n",""),lines[17].replace("\n",""),lines[19].replace("\n","")
 
